ba908923afb12973982d2f1a2c5401d3eb06d513
Ben X L <35242888+ben-excel@users.noreply.github.com>
2023-12-01 14:43:06 -0700

    Add files via upload

---
+++ video_conversion.py
@@ -0,0 +1,25 @@
+import os
+from moviepy.editor import VideoFileClip
+
+def convert_video(input_filename, output_filename):
+    # Get the current working directory
+    current_directory = os.getcwd()
+
+    # Define the full file paths for input and output
+    input_path = os.path.join(current_directory, input_filename)
+    output_path = os.path.join(current_directory, output_filename)
+
+    # Load the video
+    clip = VideoFileClip(input_path)
+    
+    # Resize the video to 16x12 pixels
+    resized_clip = clip.resize((8, 6))
+
+    # Set the frame rate to 3 fps
+    final_clip = resized_clip.set_fps(3)
+
+    # Save the modified video
+    final_clip.write_videofile(output_path, codec='libx264')
+
+# Replace 'Bad Apple.mp4' and 'Bad Apple (low res).mp4' with your file names
+convert_video('Bad Apple.mp4', 'Bad Apple (low res) 2.mp4')
---
+++ video_to_array.py
@@ -0,0 +1,44 @@
+import cv2
+import os
+
+# Function to process video frames and export brightness information to a text file
+def process_video(video_filename, output_filename):
+    video_path = os.path.join(os.getcwd(), video_filename)
+    cap = cv2.VideoCapture(video_path)
+    brightness_data = []
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        
+        if not ret:
+            break
+        
+        # Convert frame to grayscale for better brightness analysis
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+        # Analyze brightness per pixel
+        brightness_row = ""
+        for y in range(gray_frame.shape[0]):
+            for x in range(gray_frame.shape[1]):
+                pixel_brightness = gray_frame[y, x]
+                
+                # Set threshold for brightness (adjust as needed)
+                threshold = 127
+                if pixel_brightness > threshold:
+                    brightness_row += "1"
+                else:
+                    brightness_row += "0"
+        
+        brightness_data.append(brightness_row)
+    
+    cap.release()
+    
+    # Write the brightness data into a text file
+    with open(output_filename, 'w') as file:
+        for row in brightness_data:
+            file.write(f'"{row}",\n')
+
+# Provide the filename of the video in the current directory
+video_filename = "Bad Apple (low res) 2.mp4"
+output_filename = "data3.txt"
+process_video(video_filename, output_filename)
